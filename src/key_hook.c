a4d91b5f52319db5c6e3a370c1708cf1f58758f2
Futa Watanabe <fwatanab@c1r16s1.42tokyo.jp>
2023-07-27 15:08:03 +0900

    42header

--- src/key_hook.c
+++ src/key_hook.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_hook.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwatanab <fwatanab@student.42.jp>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 15:04:37 by fwatanab          #+#    #+#             */
+/*   Updated: 2023/07/27 15:07:41 by fwatanab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/so_long.h"
 
 int	close_window(t_vars *vars)
@@ -21,9 +33,10 @@ static int	key_hook(int keycode, t_vars *vars)
 		movement_player(vars, 'd');
 	return (0);
 }
+
 void	hook(t_vars *vars)
 {
 	vars->walk_count = 0;
-	mlx_hook(vars->mlx_win, 2, 1L<<0, key_hook, vars);
-	mlx_hook(vars->mlx_win, 17, 1L<<0, close_window, vars);
+	mlx_hook(vars->mlx_win, 2, 1L << 0, key_hook, vars);
+	mlx_hook(vars->mlx_win, 17, 1L << 0, close_window, vars);
 }

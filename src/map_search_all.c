d21996092ee092108fab3af24d2620e4635247a4
fwatanab <fwatanab@student.42tokyo.jp>
2023-07-27 15:30:26 +0900

    test

--- src/map_search_all.c
+++ src/map_search_all.c
@@ -19,7 +19,25 @@ void	map_search_all(char **argv, t_vars vars)
 	vars = import_map(argv, vars);
 	vars = set_null(vars);
 	player = search_player(&vars);
-	while (vars.map[player.p_y][playerp_x] != '1')
-		player.p_y++;
+	while (vars.map[player.p_y][player.p_x] != '1')
+	{
+		if (vars.c_count == 0 && vars.map[player.p_y][player.p_x] == 'E')
+		{
+			all_free(vars.map);
+			return ;
+		}
+		else if (vars.map[player.p_y][player.p_x] == 'C')
+		{
+//			player.visited[player.p_y][player.p_x] = 1;
+			vars.c_count--;
+			player.p_y++;
+		}
+		else if (vars.map[player.p_y][player.p_x] != 'E')
+		{
+//			player.visited[player.p_y][player.p_x] = 1;
+			player.p_y++;
+		}
+	}
 	all_free(vars.map);
+//	error();
 }
